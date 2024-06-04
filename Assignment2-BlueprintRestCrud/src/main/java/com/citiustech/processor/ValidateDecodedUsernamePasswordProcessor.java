7e9e574ab02c12a9d4f85265eaa6c967d22faea6
Tanmay <tanmaypatil0714@gmail.com>
2024-06-04 12:12:45 +0530

    add loggers into processor

--- Assignment2-BlueprintRestCrud/src/main/java/com/citiustech/processor/ValidateDecodedUsernamePasswordProcessor.java
+++ Assignment2-BlueprintRestCrud/src/main/java/com/citiustech/processor/ValidateDecodedUsernamePasswordProcessor.java
@@ -6,8 +6,12 @@ import java.util.Map;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ValidateDecodedUsernamePasswordProcessor implements Processor {
+
+	private static Logger logger = LoggerFactory.getLogger(ValidateDecodedUsernamePasswordProcessor.class);
 
 	private Map<String, String> credentials;
 
@@ -21,12 +25,11 @@ public class ValidateDecodedUsernamePasswordProcessor implements Processor {
 	public void process(Exchange exchange) throws Exception {
 
 		String authHeader = exchange.getIn().getHeader("Authorization", String.class);
-		System.out.println(authHeader);
 
-		// Removing the prefix "Basic " from the Base64 string
+//		Removing the prefix "Basic " from the Base64 string
 		String encodedString = authHeader.replace("Basic ", "");
 		String decodedUsernamePassword = new String(Base64.getDecoder().decode(encodedString));
-		System.out.println(decodedUsernamePassword);
+//      Splitting the string with ":"
 		String[] tokens = decodedUsernamePassword.split(":");
 
 		String username = tokens[0];
@@ -34,7 +37,13 @@ public class ValidateDecodedUsernamePasswordProcessor implements Processor {
 
 		boolean isUsernameAndPasswordMatches = credentials.entrySet().stream()
 				.anyMatch(credential -> credential.getKey().equals(username) && credential.getValue().equals(password));
-		exchange.getIn().setHeader("authorized", isUsernameAndPasswordMatches);
+		if (isUsernameAndPasswordMatches) {
+			logger.info("User authenticated Successfully");
+			exchange.getIn().setHeader("authorized", isUsernameAndPasswordMatches);
+		} else {
+			logger.error("User entered bad credentials");
+		}
+
 	}
 
 }
